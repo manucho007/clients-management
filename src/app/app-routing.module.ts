89e5fd4f30e766488c647cb54cb428d8fd88406d
Manuel Rodriguez <manrodri92@gmail.com>
2021-04-07 22:34:48 +0300

    Added Auth Guard in 2 different ways

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,8 @@
 import { NgModule } from '@angular/core';
+import {
+  AngularFireAuthGuard,
+  redirectUnauthorizedTo,
+} from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { AddClientComponent } from './components/add-client/add-client.component';
 import { ClientDetailsComponent } from './components/client-details/client-details.component';
@@ -8,20 +12,72 @@ import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { AuthGuard } from './guards/auth.guard';
+
+// FireAuthGuard custom variables
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/login']);
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
+  {
+    path: '',
+    component: DashboardComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'client/add', component: AddClientComponent },
-  { path: 'client/edit/:id', component: EditClientComponent },
-  { path: 'client/:id', component: ClientDetailsComponent },
-  { path: 'settings', component: SettingsComponent },
+  {
+    path: 'client/add',
+    component: AddClientComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  {
+    path: 'client/edit/:id',
+    component: EditClientComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  {
+    path: 'client/:id',
+    component: ClientDetailsComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
+  },
   { path: '**', component: NotFoundComponent },
 ];
 
+// const routes: Routes = [
+//   { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
+//   { path: 'login', component: LoginComponent },
+//   { path: 'register', component: RegisterComponent },
+//   {
+//     path: 'client/add',
+//     component: AddClientComponent,
+//     canActivate: [AuthGuard],
+//   },
+//   {
+//     path: 'client/edit/:id',
+//     component: EditClientComponent,
+//     canActivate: [AuthGuard],
+//   },
+//   {
+//     path: 'client/:id',
+//     component: ClientDetailsComponent,
+//     canActivate: [AuthGuard],
+//   },
+//   { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
+//   { path: '**', component: NotFoundComponent },
+// ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
