3db05d8f7c9c2c147697c4d64ee58e6905b688b3
Manuel Rodriguez <manrodri92@gmail.com>
2021-04-08 09:51:12 +0300

    Added panel to change settings and add in LS

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -10,9 +10,16 @@ export class SettingsService {
     disableBalanceOnAdd: false,
     disableBalanceOnEdit: true,
   };
-  constructor() {}
+  constructor() {
+    if (localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
+  }
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
